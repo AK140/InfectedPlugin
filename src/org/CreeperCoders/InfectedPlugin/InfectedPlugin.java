efd252a18f9827bba50014257509a7b4eecbabf3
Wilee999 <Infinitywilee@rocketmail.com>
2013-07-27 11:45:47 -0700

    Actually make the class thing whatever

--- src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
+++ src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
@@ -1,1 +1,20 @@
-awesome plugin ever
+package org.CreeperCoders.InfectedPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InfectedPlugin extends JavaPlugin
+{
+   public final Logger log = Logger.getLogger("Minecraft");
+   
+   @Override
+   public void onEnable()
+   {
+       log.info("PluginPack has been enabled!");
+   }
+   
+   @Override
+   public void onDisable()
+   {
+       log.info("PluginPack has been disabled.");
+   }
+}
