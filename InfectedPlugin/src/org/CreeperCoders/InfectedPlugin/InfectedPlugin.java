35f31441949578bd51b7456a32102db8cb42ac02
Ryan <ryan.ictclubsales@gmail.com>
2013-08-12 18:59:38 +0100

    Moved the commands into the main class as else it can get into a mess without adding some form of actual command managment system.

--- InfectedPlugin/src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
+++ InfectedPlugin/src/org/CreeperCoders/InfectedPlugin/InfectedPlugin.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.CreeperCoders.InfectedPlugin.Commands.pluginpack;
 import org.CreeperCoders.InfectedPlugin.Commands.anticheat;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class InfectedPlugin extends JavaPlugin
 {
@@ -32,6 +35,27 @@ public class InfectedPlugin extends JavaPlugin
     public void onDisable()
     {
         log.info(String.format("[%s] has been disabled!", getDescription().getName()));
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        Player player = (Player) sender;
+
+        if (commandLabel.equalsIgnoreCase("anticheat"))
+        {
+
+            sender.sendMessage(ChatColor.GREEN + "Anticheat 1.0 is working 100%");
+            return true;
+
+        }
+
+        if (commandLabel.equalsIgnoreCase("pluginpack"))
+        {
+            sender.sendMessage(ChatColor.GREEN + "PluginPack 1.0, working 100%! Use /anticheat to see anticheat details");
+            return true;
+        }
+        return false;
     }
 
 }
