0e793c9907ab3b580be543b2c3a04380b4697cbc
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-27 16:22:26 -0700

    Added IP_Command, just for the classes that need it. !!!!!!DO NOT USE COMMIT!!!!!
    Use the next commit, it fixes problems.

---
+++ src/org/CreeperCoders/InfectedPlugin/Commands/IP_Command.java
@@ -0,0 +1,27 @@
+package org.CreeperCoders.InfectedPlugin.Commands;
+
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+
+public class IP_Command
+{
+    public Player getPlayer(final String partialname) throws PlayerNotFoundException
+    {
+        List<Player> matches = Bukkit.matchPlayer(partialname);
+        if (matches.isEmpty())
+        {
+            for (Player player : Bukkit.getOnlinePlayers())
+            {
+                if (player.getDisplayName().toLowerCase().contains(partialname.toLowerCase()))
+                {
+                    return player;
+                }
+            }
+            throw new PlayerNotFoundException(partialname);
+        }
+        else
+        {
+            return matches.get(0);
+        }
+    }
+}
