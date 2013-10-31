d16b2a20a4775a7875a353307506b67bb643e544
Ryan <ryan.ictclubsales@gmail.com>
2013-10-31 16:59:30 +0000

    Removed the listeners, they do nothing, so why keep them there?

--- src/org/CreeperCoders/InfectedPlugin/Listeners/IP_PlayerListener.java
+++
@@ -1,8 +0,0 @@
-package org.CreeperCoders.InfectedPlugin.Listeners;
-
-import org.bukkit.event.Listener;
-
-public class IP_PlayerListener implements Listener
-{
-    //Reserved for future use :)
-}
