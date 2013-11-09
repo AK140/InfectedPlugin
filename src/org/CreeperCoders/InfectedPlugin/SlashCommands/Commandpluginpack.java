8e18e2ffecd38651d7f0595ef4a74ef921cb2702
Wilee999 <Infinitywilee@rocketmail.com>
2013-11-08 16:21:23 -0800

    whoops

--- src/org/CreeperCoders/InfectedPlugin/SlashCommands/Commandpluginpack.java
+++ src/org/CreeperCoders/InfectedPlugin/SlashCommands/Commandpluginpack.java
@@ -1,11 +1,11 @@
 package org.CreeperCoders.InfectedPlugin.SlashCommands;
 
-import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.CreeperCoders.InfectedPlugin.g;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Command_ender extends TFM_Command
+public class Commandpluginpack extends IP_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
